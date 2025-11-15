d25e8daa7414fbf1daf3845d1609c55bf6deb10c
George <george.bobovsky@gmail.com>
2025-11-15 10:09:10 +0500

    f-4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import AuthStatusControl from "../authStatusControl/AuthStatusControl";
 import CartToggle from "../cartToggle/CartToggle";
 import SidebarToggle from "../sidebarToggle/SidebarToggle";
 import ThemeColorToggle from "../themeColorToggle/ThemeColorToggle";
 import styles from "./Header.module.css";
 import classNames from "classnames";
+import { useCallback } from "react";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const onPointerDown = useCallback((event) => event.preventDefault(), []);
+  const onClick = useCallback(() => navigate("/"), [navigate]);
+
   return (
     <header className={classNames(styles.header, "clearfix")}>
       <h1
-        onPointerDown={(event) => event.preventDefault()}
+        onPointerDown={onPointerDown}
+        onClick={onClick}
         className={styles.title}
       >
         Food App
