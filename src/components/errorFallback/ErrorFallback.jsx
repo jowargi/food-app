724b69b9ced4a5f22447a26cc62f612c3ac4f10f
George <george.bobovsky@gmail.com>
2025-11-17 11:39:06 +0500

    f-7

--- src/components/errorFallback/ErrorFallback.jsx
+++ src/components/errorFallback/ErrorFallback.jsx
@@ -1,8 +1,25 @@
+import { useThemeColorContext } from "../themeColorContextProvider/ThemeColorContextProvider";
+import styles from "./ErrorFallback.module.css";
+import classNames from "classnames";
+
 export default function ErrorFallback({ name, message }) {
+  const { themeColor } = useThemeColorContext();
+
   return (
-    <div>
-      <p>{name}</p>
-      <p>{message}</p>
+    <div
+      className={classNames(
+        styles.container,
+        styles[`container--${themeColor}`]
+      )}
+    >
+      <h2 className={classNames(styles.name, styles[`name--${themeColor}`])}>
+        {name}
+      </h2>
+      <p
+        className={classNames(styles.message, styles[`message--${themeColor}`])}
+      >
+        {message}
+      </p>
     </div>
   );
 }
